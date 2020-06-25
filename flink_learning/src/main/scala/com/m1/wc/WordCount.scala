292df49d18d6db46327b50bfb13fa12f9f67407b
moqi <moqimoqidea@gmail.com>
2020-06-25 17:41:02 +0800

    20200625

---
+++ flink_learning/src/main/scala/com/m1/wc/WordCount.scala
@@ -0,0 +1,24 @@
+package com.m1.wc
+
+import org.apache.flink.api.scala._
+
+/**
+ * @author moqi
+ *         On 6/25/20 17:37
+ */
+object WordCount {
+
+  val inputFilePath: String = "flink_learning/src/main/resources/data.txt"
+
+  def main(args: Array[String]): Unit = {
+
+    val environment = ExecutionEnvironment.getExecutionEnvironment
+
+    val inputDataSet = environment.readTextFile(inputFilePath)
+
+    val resultDataSet = inputDataSet.flatMap(_.split(" ")).map((_, 1)).groupBy(0).sum(1)
+
+    resultDataSet.print()
+  }
+
+}
