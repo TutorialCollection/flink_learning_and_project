16798aff610fced9d3e7e00aaee8ff7d9f35d911
moqi <moqimoqidea@gmail.com>
2020-06-25 17:18:53 +0800

    20200625

--- flink_learning/src/main/scala/com/hp/wc/WordCount.scala
+++ flink_learning/src/main/scala/com/hp/wc/WordCount.scala
@@ -1,8 +1,6 @@
 package com.hp.wc
 
 import org.apache.flink.api.scala._
-import org.apache.flink.api.scala.{DataSet, ExecutionEnvironment}
-
 
 object WordCount {
   def main(args: Array[String]): Unit = {
@@ -11,9 +9,9 @@ object WordCount {
     val inputFilePath: String = "flink_learning/src/main/resources/data.txt"
     val inputDataSet: DataSet[String] = environment.readTextFile(inputFilePath)
 
-    val resultDataSet: DataSet[(String,Int)] = inputDataSet
+    val resultDataSet: DataSet[(String, Int)] = inputDataSet
       .flatMap(_.split(" "))
-      .map((_,1))
+      .map((_, 1))
       .groupBy(0)
       .sum(1)
 
